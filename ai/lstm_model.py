5f7b81fabfb9402d008bdf0e70196cd9aee97d51
TechSolute <pharshiesammy@gmail.com>
2026-03-06 22:14:39 +0100

    Update lstm_model.py

--- ai/lstm_model.py
+++ ai/lstm_model.py
@@ -1,52 +1,29 @@
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.layers import LSTM, Dense
 
+def lstm_predict(series):
 
-class LSTMForecaster:
+    X=[]
+    y=[]
 
-    def __init__(self):
-        self.model = None
-        self.scaler = MinMaxScaler()
+    for i in range(10,len(series)):
+        X.append(series[i-10:i])
+        y.append(series[i])
 
-    def train(self, series):
+    X=np.array(X)
+    y=np.array(y)
 
-        data = series.values.reshape(-1,1)
+    X=X.reshape(X.shape[0],X.shape[1],1)
 
-        scaled = self.scaler.fit_transform(data)
+    model=Sequential([
+        LSTM(32),
+        Dense(1)
+    ])
 
-        X, y = [], []
+    model.compile("adam","mse")
+    model.fit(X,y,epochs=5,verbose=0)
 
-        for i in range(10, len(scaled)):
-            X.append(scaled[i-10:i])
-            y.append(scaled[i])
+    pred=model.predict(X[-1].reshape(1,10,1))[0][0]
 
-        X = np.array(X)
-        y = np.array(y)
-
-        self.model = Sequential([
-            LSTM(64, return_sequences=True),
-            Dropout(0.2),
-            LSTM(32),
-            Dense(1)
-        ])
-
-        self.model.compile(
-            optimizer="adam",
-            loss="mse"
-        )
-
-        self.model.fit(X, y, epochs=5, verbose=0)
-
-    def predict(self, series):
-
-        seq = self.scaler.transform(
-            series.values[-10:].reshape(-1,1)
-        )
-
-        seq = seq.reshape(1,10,1)
-
-        pred = self.model.predict(seq)
-
-        return float(self.scaler.inverse_transform(pred)[0][0])
+    return pred
